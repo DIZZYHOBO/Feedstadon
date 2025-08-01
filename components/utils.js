f8343c92b03cadec54a41ec5a63c1809a63c40c9
DIZZYHOBO <dystorp@gmail.com>
2025-08-02 03:01:52 +0800

    Create utils.js

---
+++ components/utils.js
@@ -0,0 +1,27 @@
+export function formatTimestamp(isoDate) {
+    const now = new Date();
+    const postDate = new Date(isoDate);
+    const seconds = Math.floor((now - postDate) / 1000);
+
+    let interval = seconds / 31536000; // years
+    if (interval > 1) {
+        return postDate.toLocaleDateString('en-us', { month: 'short', day: 'numeric', year: 'numeric' });
+    }
+    interval = seconds / 2592000; // months
+    if (interval > 1) {
+        return postDate.toLocaleDateString('en-us', { month: 'short', day: 'numeric' });
+    }
+    interval = seconds / 86400; // days
+    if (interval > 1) {
+        return `${Math.floor(interval)}d`;
+    }
+    interval = seconds / 3600; // hours
+    if (interval > 1) {
+        return `${Math.floor(interval)}h`;
+    }
+    interval = seconds / 60; // minutes
+    if (interval > 1) {
+        return `${Math.floor(interval)}m`;
+    }
+    return `${Math.floor(seconds)}s`;
+}
