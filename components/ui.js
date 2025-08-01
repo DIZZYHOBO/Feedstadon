08d6a6354f28199e111e5053fe797ecb1e9724b4
DIZZYHOBO <dystorp@gmail.com>
2025-08-01 20:27:50 +0800

    Create ui.js

---
+++ components/ui.js
@@ -0,0 +1,20 @@
+export const ICONS = {
+    notification: `<svg class="icon" viewBox="0 0 24 24" style="width:24px; height:24px; fill:currentColor;"><path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/></svg>`,
+    reply: `<svg class="icon" viewBox="0 0 24 24"><path d="M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11z"/></svg>`,
+    boost: `<svg class="icon" viewBox="0 0 24 24"><path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z"/></svg>`,
+    favorite: `<svg class="icon" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>`,
+    bookmark: `<svg class="icon" viewBox="0 0 24 24"><path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/></svg>`
+};
+
+const modal = document.getElementById('modal');
+const modalBody = document.getElementById('modal-body');
+
+export function showModal(contentNode) {
+    modalBody.innerHTML = '';
+    modalBody.appendChild(contentNode);
+    modal.classList.add('visible');
+}
+export function hideModal() {
+    modal.classList.remove('visible');
+}
+modal.addEventListener('click', (e) => { if (e.target === modal) hideModal(); });
