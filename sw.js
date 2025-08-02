624ceaa612e88bf81e2036693b5ae32e78e07056
DIZZYHOBO <dystorp@gmail.com>
2025-08-02 23:19:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MODIFIED: The cache name now includes a version number.
-const CACHE_NAME = 'feedstadon-v4.2.9'; 
+const CACHE_NAME = 'feedstadon-v4.3.0'; 
 const urlsToCache = [
   './',
   './index.html',
