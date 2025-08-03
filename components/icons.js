00f845fab92d49271bb1d0d6174c19cd5f5b5a09
DIZZYHOBO <dystorp@gmail.com>
2025-08-04 02:24:46 +0800

    Update icons.js

--- components/icons.js
+++ components/icons.js
@@ -11,5 +11,7 @@ export const ICONS = {
     message: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M20,2H4A2,2 0 0,0 2,4V22L6,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2Z" /></svg>`,
     notifications: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9m-4.27 13a2 2 0 0 1-3.46 0"></path></svg>`,
     lemmyUpvote: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M12,2L2,12H7V22H17V12H22L12,2Z" /></svg>`,
-    lemmyDownvote: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M12,22L22,12H17V2H7V12H2L12,22Z" /></svg>`
+    lemmyDownvote: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M12,22L22,12H17V2H7V12H2L12,22Z" /></svg>`,
+    edit: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.13,5.12L18.88,8.87M3,17.25V21H6.75L17.81,9.94L14.06,6.19L3,17.25Z" /></svg>`,
+    delete: `<svg class="icon" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`
 };
