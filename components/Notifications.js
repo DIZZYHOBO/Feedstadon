df40cd5f3502dda4043c432b55ed810ba12c993f
DIZZYHOBO <dystorp@gmail.com>
2025-08-05 23:53:31 +0800

    Update Notifications.js

--- components/Notifications.js
+++ components/Notifications.js
@@ -93,8 +93,8 @@ export async function renderNotificationsPage(state, actions) {
         let lemmyNotifs = [];
         const lemmyInstance = localStorage.getItem('lemmy_instance');
         if (lemmyInstance) {
-            const repliesResponse = await apiFetch(lemmyInstance, null, '/api/v3/user/replies?sort=New&unread_only=false', {}, 'lemmy');
-            const mentionsResponse = await apiFetch(lemmyInstance, null, '/api/v3/user/mentions?sort=New&unread_only=false', {}, 'lemmy');
+            const repliesResponse = await apiFetch(lemmyInstance, null, '/api/v3/user/reply?sort=New&unread_only=false', {}, 'lemmy');
+            const mentionsResponse = await apiFetch(lemmyInstance, null, '/api/v3/user/mention?sort=New&unread_only=false', {}, 'lemmy');
             
             lemmyNotifs = [
                 ...repliesResponse.data.replies.map(r => ({...r, type: 'reply'})),
